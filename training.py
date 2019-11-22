380f2f382dd3f2f858c9819824973d5dfa668abf
Xavier Pellerin <xavier.peller1@gmail.com>
2019-11-22 16:59:48 +0100

    3. Define the set_classifier() +    Write training_with_nogrid() and check_parameters()

--- training.py
+++ training.py
@@ -31,9 +31,13 @@
 import numpy as np
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import cross_val_score, cross_val_predict, StratifiedShuffleSplit
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier
 
 # -------------------------
 # ------ FUNCTIONS --------
@@ -108,7 +112,7 @@ def format_dataset(path_raw_data, raw_classif=None):
     return features_data, coord, hght, trgt
 
 
-def split_dataset(data_values, target_values, rd_state=0, train_ratio=0.8, test_ratio=0.2, threshold=500000):
+def split_dataset(data_values, target_values, train_ratio=0.8, test_ratio=0.2, threshold=500000):
     """
     Split the input data and target in data_train, data_test, target_train and target_test.
     Check the length of the dataset. If length > 500 kpts, trainset = 400 kpts and testset = 100 kpts.
@@ -122,14 +126,23 @@ def split_dataset(data_values, target_values, rd_state=0, train_ratio=0.8, test_
     :return: data_train, data_test, target_train and target_test as np.ndarray.
     """
     # Check if dataset > 500 kpts
-    if len(data_values[:, 0]) > threshold:
-        train_ratio = train_ratio * threshold
-        test_ratio = test_ratio * threshold
+    n_samples = len(data_values[:, 0])
+    if n_samples > threshold:
+        train_size = int(train_ratio * threshold)
+        test_size = int(test_ratio * threshold)
+    else:
+        train_size = int(train_ratio * n_samples)
+        test_size = int(test_ratio * n_samples)
 
     data_train, data_test, target_train, target_test = train_test_split(data_values, target_values,
-                                                                        random_state=rd_state,
-                                                                        train_size=train_ratio,
-                                                                        test_size=test_ratio)
+                                                                        random_state=0,
+                                                                        train_size=train_size,
+                                                                        test_size=test_size,
+                                                                        stratify=target_values)
+    # Convert target_train and target_test column-vectors as 1d array
+    target_train = target_train.reshape(train_size)
+    target_test = target_test.reshape(test_size)
+
     return data_train, data_test, target_train, target_test
 
 
@@ -159,6 +172,118 @@ def scale_dataset(data_to_scale, method='Standard'):
     return data_scaled
 
 
+def check_parameters(classifier, fit_params):
+    """
+    Check if the given parameters match with the given classifier.
+    :param classifier: The given classifier.
+    :param fit_params: Parameters to check in dict.
+    :return: classifier: The setting up classifier.
+    """
+    # Get the type of classifier
+    clf_name = str(type(classifier)).split('.')[-1][:-2]
+
+    # Check if the parameters are valid for the given classifier
+    for key in fit_params.keys():
+        try:
+            temp_dict = {key: fit_params[key]}
+            classifier.set_params(**temp_dict)
+        except ValueError:
+            print("\nValueError: Invalid parameter '{}' for {}, "
+                  "it has been ignored!".format(str(key), clf_name))
+
+    return classifier
+
+
+def set_random_forest(fit_params=None):
+    """
+    Set the learning algorithm as RandomForestClassier.
+    :param fit_params: A dict with the parameters to set up.
+    :return: classifier: the desired classifier with the required parameters
+    """
+    # Set the classifier
+    if isinstance(fit_params, dict):
+        fit_params['random_state'] = 0
+        classifier = RandomForestClassifier()
+        classifier = check_parameters(classifier, fit_params)
+
+    else:
+        classifier = RandomForestClassifier(n_estimators=100,
+                                            max_depth=5,
+                                            min_samples_leaf=500,
+                                            n_jobs=-1,
+                                            random_state=0)
+
+    return classifier
+
+
+def set_gradient_boosting(fit_params=None):
+    """
+    Set the learning algorithm as GradientBoostingClassifier.
+    :param fit_params: A dict with the parameters to set up.
+    :return: classifier: the desired classifier with the required parameters
+    """
+    # Set the classifier
+    if isinstance(fit_params, dict):
+        fit_params['random_state'] = 0
+        classifier = GradientBoostingClassifier()
+        classifier = check_parameters(classifier, fit_params)
+    else:
+        classifier = GradientBoostingClassifier(loss='deviance',
+                                                n_estimators=10,
+                                                max_depth=3,
+                                                min_samples_leaf=1000,
+                                                random_state=0)
+
+    return classifier
+
+
+def set_linear_svc(fit_params=None):
+    """
+    Set the learning algorithm as LinearSVC.
+    :param fit_params: A dict with the parameters to set up.
+    :return: classifier: the desired classifier with the required parameters
+    """
+    # Set the classifier
+    if isinstance(fit_params, dict):
+        fit_params['random_state'] = 0
+        fit_params['dual'] = False
+        classifier = LinearSVC()
+        classifier = check_parameters(classifier, fit_params)
+    else:
+        classifier = LinearSVC(penalty='l2',
+                               loss='hinge',
+                               dual=False,
+                               C=1.0,
+                               multi_class='ovr',
+                               max_iter=1000,
+                               random_state=0)
+
+    return classifier
+
+
+def set_mlp_classifier(fit_params=None):
+    """
+    Set the learning algorithm as MLPClassifier.
+    :param fit_params: A dict with the parameters to set up.
+    :return: classifier: the desired classifier with the required parameters
+    """
+    # Set the classifier
+    if isinstance(fit_params, dict):
+        fit_params['random_state'] = 0
+        classifier = MLPClassifier()
+        classifier = check_parameters(classifier, fit_params)
+
+    else:
+        classifier = MLPClassifier(hidden_layer_sizes=(50, 50),
+                                   activation='relu',
+                                   solver='adam',
+                                   alpha=0.0001,
+                                   max_iter=200,
+                                   random_state=0)
+
+    return classifier
+
+
 def training_with_grid(learning_algo, training_data, training_target, param_grid=None):
     """
     Train model with GridSearchCV meta-estimator according the chosen learning algorithm.
@@ -170,16 +295,25 @@ def training_with_grid(learning_algo, training_data, training_target, param_grid
     """
 
 
-def training_with_nogrid(learning_algo, training_data, training_target):
+def training_with_nogrid(classifier, training_data, training_target):
     """
-    Train model without GridSearchCV meta-estimator according the chosen learning algorithm.
-    This function performs cross-validation.
-    :param learning_algo: Set the algorithm to train the model.
+    Train model with cross-validation according the chosen classifier.
+    :param classifier: Set the algorithm to train the model.
     :param training_data: The training dataset.
     :param training_target: The targets corresponding to the training dataset.
-    :return:
+    :return: model: the training model.
     """
+    # Set cross_validation method with train_size 80% and validation_size 20%
+    cross_val = StratifiedShuffleSplit(n_splits=5, train_size=0.8, test_size=0.2, random_state=0)
 
+    # Get the training scores
+    training_scores = cross_val_score(classifier, training_data, training_target, cv=cross_val, n_jobs=-1)
+    print("Training model scores performed with cross-validation:\n{}\n".format(training_scores))
+
+    # Set the classifier with training_data and target
+    classifier.fit(training_data, training_target)
+
+    return classifier
 
 # -------------------------
 # --------- MAIN ----------
@@ -187,9 +321,20 @@ def training_with_nogrid(learning_algo, training_data, training_target):
 
 
 if __name__ == '__main__':
+    # Learning algorithm 'rf', 'gb', 'svm', 'ann'
+    algo = 'ann'
+    parameters = {"loss": 'deviance',
+                  "n_estimators": 50,
+                  "max_depth": 3,
+                  "min_samples_leaf": 1000,
+                  "n_jobs": -1,
+                  "random_state": 0}
+
+    # With or without GridSearchCV
+    grid = False
     # path to the CSV file
-    # raw_data = "D:/PostDoc/Python/DataTest/20150603_Classif_plus_Geom_50kpts.csv"
-    raw_data = "D:/PostDoc/Python/DataTest/20150603_targeted_nantest_10kpts.csv"  # Test file with many nan values
+    raw_data = "D:/PostDoc/Python/DataTest/20150603_Classif_plus_Geom_50kpts.csv"
+    # raw_data = "D:/PostDoc/Python/DataTest/20150603_targeted_nantest_10kpts.csv"  # Test file with many nan values
 
     # Format the data as data / XY / Z / target DataFrames
     data, xy_coord, z_height, target = format_dataset(raw_data, raw_classif='lassif')
@@ -198,10 +343,23 @@ if __name__ == '__main__':
     data = scale_dataset(data, method='Standard')
 
     # Create samples for training and testing
-    X_train, X_test, y_train, y_test = split_dataset(data.values, target.values, rd_state=0)
+    X_train_val, X_test, y_train_val, y_test = split_dataset(data.values, target.values)
 
-    # Learning algorithm 'rf', 'gb', 'svm', 'ann'
-    algo = 'rf'
+    # Set the chosen learning classifier
+    if algo is 'rf':
+        clf = set_random_forest(parameters)
+    elif algo is 'gb':
+        clf = set_gradient_boosting(parameters)
+    elif algo is 'svm':
+        clf = set_linear_svc(parameters)
+    elif algo is 'ann':
+        clf = set_mlp_classifier()
 
-    # With GridSearch or not
-    grid = True
+    # What type of training
+    if grid:
+        print('Training with grid')
+    else:
+        # Train model with cross_val and train_val datasets
+        model = training_with_nogrid(clf, X_train_val, y_train_val)
+        print(model)
+
