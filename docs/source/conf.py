3cbe6bece38b421a706a55573be9696e5ef85772
Xavier PELLERIN <xavier.peller1@gmail.com>
2023-01-23 22:56:25 +0100

    Update conf.py
    Comment 'html_static_path = ['_static']'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
