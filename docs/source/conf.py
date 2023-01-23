12047e917d7b7d8a1e5850e418c34df69d6655c5
Xavier PELLERIN <xavier.peller1@gmail.com>
2023-01-23 22:47:04 +0100

    Update conf.py
    Add extensions ext.duration, ext.autodoc, ext.autosummary, and change theme for 'sphinx_rtd_theme'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,11 +10,16 @@ project = 'cLASpy_T'
 copyright = '2023, Xavier PELLERIN LE BAS'
 author = 'Xavier PELLERIN LE BAS'
 release = '0.2'
+version = '0.2.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +29,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
